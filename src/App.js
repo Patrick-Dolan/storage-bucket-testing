4ebb40813f8d0833ff75ff75265baf4b6725cb56
Patrick Dolan <dolanp1992@gmail.com>
2022-08-21 16:19:32 -0700

    feature: add file upload capability to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import { storage } from "./firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
+
 function App() {
+
+  const [fileUpload, setFileUpload] = useState(null);
+
+  const uploadFile = () => {
+    if (fileUpload == null) return null;
+    // Get reference to fill path on storage bucket
+    const fileRef = ref(storage, `files/${v4() + fileUpload.name}`);
+    // Upload the file
+    uploadBytes(fileRef, fileUpload).then(() => {
+      alert("Image uploaded");
+    });
+  };
+
   return (
     <>
-      <h1>Main App Component</h1>
+      <h1>Storage Upload Testing</h1>
+      <input type="file" onChange={(event) => {setFileUpload(event.target.files[0])}} />
+      <button onClick={uploadFile}>Upload</button>
     </>
   );
 }
